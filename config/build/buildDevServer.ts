c83e7179e06bc2a2aad2d0f74642d128e89e43e6
Danil Chernyh <dchernyh@syntellect.ru>
2023-02-08 10:50:51 +0500

    #15 Добавлен плагин для быстрых изменений

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -8,5 +8,6 @@ export function buildDevServer(options: BuildOptions): DevServerConfiguration {
         // Если true: Открывает автоматически страницу в браузере
         open: true,
         historyApiFallback: true,
+        hot: true,
     }
 }
