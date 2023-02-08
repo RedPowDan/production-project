66d031b004922a3dae4a4dc46cd8589197829bbf
Danil Chernyh <dchernyh@syntellect.ru>
2023-02-08 15:13:28 +0500

    #20 Добавлены тесты

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,46 @@
+import { classNames } from './classNames';
+
+describe('Тестирование функции classNames', () => {
+    test('Для одного класса', () => {
+        expect(classNames('someClass')).toBe('someClass');
+    });
+
+    test('С модами', () => {
+        expect(
+            classNames(
+                'someClass',
+                { someClass1: true },
+            ),
+        ).toBe('someClass someClass1');
+    });
+
+    test('С модами и доп. параметрами', () => {
+        const expected = 'someClass someClass2 someClass1';
+        expect(
+            classNames(
+                'someClass',
+                { someClass1: true },
+                ['someClass2'],
+            ),
+        ).toBe(expected);
+    });
+
+    test('С отключенными модами', () => {
+        expect(
+            classNames(
+                'someClass',
+                { someClass1: false, someClass2: undefined },
+            ),
+        ).toBe('someClass');
+    });
+
+    test('С undefined парамтерами', () => {
+        expect(
+            classNames(
+                'someClass',
+                {},
+                [undefined, null],
+            ),
+        ).toBe('someClass');
+    });
+});
