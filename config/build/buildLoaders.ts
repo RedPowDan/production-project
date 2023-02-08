c5dc1af5b21d44add28caaf7bd5067eb5c0ad915
Danil Chernyh <dchernyh@syntellect.ru>
2023-02-08 11:12:28 +0500

    #16 Добавлен babel и extractPlugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -6,6 +6,26 @@ import { BuildOptions }     from './types/config';
 
 export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
 
+    const babelLoader = {
+        test: /\.(js|jsx|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+            loader: "babel-loader",
+            options: {
+                presets: ['@babel/preset-env'],
+                plugins: [
+                    [
+                        "i18next-extract",
+                        {
+                            locales: ['ru', 'en'],
+                            keyAsDefaultValue: true
+                        }
+                    ]
+                ]
+            }
+        }
+    }
+
     const cssLoader = {
         test: /\.s[ac]ss$/i,
         use: [
@@ -49,7 +69,10 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
         ]
     }
 
+
+    // Последовательность важна
     return [
+        babelLoader,
         typescriptLoader,
         cssLoader,
         svgLoader,
